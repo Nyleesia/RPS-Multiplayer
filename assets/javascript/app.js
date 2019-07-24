8c51d4f0c7acc9a086ff179b3f915a8151e88c3b
Nyleesia Nicome Schnarr <nyleesia.nicome@users.noreply.github.com>
2019-07-23 23:46:41 -0400

    items to update homework

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,53 +1,82 @@
-$(document).ready(function(){
-
-//Initializes the firebase VARIABLE
-let database = firebase.database();
-console.log(database);
-
-//Firebase configuration
-  let firebaseConfig = {
+$(document).ready(function() {
+  let config = {
     apiKey: "AIzaSyDs1deI2cIl0lO8VHq4yzs2O-ONILbaoCI",
     authDomain: "rps-nschnarr.firebaseapp.com",
     databaseURL: "https://rps-nschnarr.firebaseio.com",
     projectId: "rps-nschnarr",
     storageBucket: "",
     messagingSenderId: "832432259312",
-    appId: "1:832432259312:web:b1f69c7b7bd2e979"
   };
+    // Initialize Firebase
+    firebase.initializeApp(config);
 
-//Initializes the firebase DATABASE
-firebase.initializeApp(firebaseConfig);
-console.log(firebaseConfig);
+    // Reference the database
+    let database= firebase.database();
+        
+    //Collecting user data for messaging and player info
+    //Collects the user information in a form (user name)
+    // Initiates user data variables
+    let email="";
+    let username = "";
+    let password = "";
+    let userId = 0;
+    // Storing the snapshot.val() in a variable
+   
 
-//Initializes variables to reference items in the firebase database
-let playersRef = db.ref('players'); 
-let connectionsRef = db.ref("connections"); 
-let connectedRef = db.ref(".info/connected");
-let p1Ref = playersRef.child('player1'); 
-let p2Ref = playersRef.child('player2');
-let turnRef = db.ref('turn');
-let winsRef = db.ref('wins');
-let lossesRef = db.ref('losses');
-let chatRef = db.ref('chat');
+    $("#userSubmit").on("click", function(event){
+            event.preventDefault();
+            let snap = snapshot.val();
+            // References values using IDs
+            email=$("#email").val().trim();
+            username=$("#username").val().trim();
+            password=$("#password").val().trim();
+            userId = Math.floor(Math.random()*100000);
 
+            // Pushes a user data object for each new player
+            database.ref("users").push({
+                email:email,
+                username: username,
+                password: password,
+                userId: userId,
+                });
+        });
 
- // Initializes variables to take in and compare inputs and results for each player
+        //Looks for additional data added amnd appends it to the users list
+        database.ref().on("child_added", function(snapshot) {
+            // Storing the snapshot.val() in a variable
+            var snap = snapshot.val();
+            // Console.loging the last user's data
+            console.log(snap.email);
+            console.log(snap.username);
+            console.log(snap.password);
+            console.log(snap.userId);
+        }, 
+        function(errorObject) {
+            console.log("Error: " + errorObject.code);
+        });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        $("#submitMsg").on("click", function(event){
+            event.preventDefault();
+            // References values using IDs
+            messageText=$("#usermsg").val();
+            
+            // Pushes a user data object for each new player
+            database.ref("messages").push({
+                messageText:messageText,
+            });
+    });
+    database.ref().on("child_added", function(snapshot) {
+        // Storing the snapshot.val() in a variable
+        var snap = snapshot.val();
+        // Console.loging the last user's data
+        console.log(snap.username);
+        console.log(snap.messageText);
+    }, 
+    function(errorObject) {
+        console.log("Couldn't read message: " + errorObject.code);
+    });
 });
+
+
+// Game starts here------------------------------------------------
+
